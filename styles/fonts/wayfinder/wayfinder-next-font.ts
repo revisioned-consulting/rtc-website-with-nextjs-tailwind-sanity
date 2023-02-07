ffc4c8c64ef24923574676e553b0d89716b78232
George Bockari <bitwhys@gmail.com>
2023-02-07 03:12:50 -0500

    fix(Wayfinder font): change path names

--- styles/fonts/wayfinder/wayfinder-next-font.ts
+++ styles/fonts/wayfinder/wayfinder-next-font.ts
@@ -1,61 +1,61 @@
-import localFont from "@next/font/local";
+import localFont from '@next/font/local'
 
 export const wayfinderFont = localFont({
   src: [
-    { path: "./WayFinderCF-Thin.eot", weight: "100" },
-    { path: "./WayFinderCF-Thin.woff", weight: "100" },
-    { path: "./WayFinderCF-Thin.woff2", weight: "100" },
+    { path: './WayfinderCF-Thin.eot', weight: '100' },
+    { path: './WayfinderCF-Thin.woff', weight: '100' },
+    { path: './WayfinderCF-Thin.woff2', weight: '100' },
     {
-      path: "./WayFinderCF-Regular.eot",
-      weight: "400",
+      path: './WayfinderCF-Regular.eot',
+      weight: '400',
     },
     {
-      path: "./WayFinderCF-Regular.woff",
-      weight: "400",
+      path: './WayfinderCF-Regular.woff',
+      weight: '400',
     },
     {
-      path: "./WayFinderCF-Regular.woff2",
-      weight: "400",
+      path: './WayfinderCF-Regular.woff2',
+      weight: '400',
     },
     {
-      path: "./WayFinderCF-DemiBold.eot",
-      weight: "600",
+      path: './WayfinderCF-DemiBold.eot',
+      weight: '600',
     },
     {
-      path: "./WayFinderCF-DemiBold.woff",
-      weight: "600",
+      path: './WayfinderCF-DemiBold.woff',
+      weight: '600',
     },
     {
-      path: "./WayFinderCF-DemiBold.woff2",
-      weight: "600",
+      path: './WayfinderCF-DemiBold.woff2',
+      weight: '600',
     },
-    { path: "./WayFinderCF-Bold.eot", weight: "700" },
-    { path: "./WayFinderCF-Bold.woff", weight: "700" },
-    { path: "./WayFinderCF-Bold.woff2", weight: "700" },
+    { path: './WayfinderCF-Bold.eot', weight: '700' },
+    { path: './WayfinderCF-Bold.woff', weight: '700' },
+    { path: './WayfinderCF-Bold.woff2', weight: '700' },
     {
-      path: "./WayFinderCF-ExtraBold.eot",
-      weight: "800",
-    },
-    {
-      path: "./WayFinderCF-ExtraBold.woff",
-      weight: "800",
+      path: './WayfinderCF-ExtraBold.eot',
+      weight: '800',
     },
     {
-      path: "./WayFinderCF-ExtraBold.woff2",
-      weight: "800",
+      path: './WayfinderCF-ExtraBold.woff',
+      weight: '800',
     },
-    { path: "./WayFinderCF-Heavy.eot", weight: "900" },
-    { path: "./WayFinderCF-Heavy.woff", weight: "900" },
     {
-      path: "./WayFinderCF-Heavy.woff2",
-      weight: "900",
+      path: './WayfinderCF-ExtraBold.woff2',
+      weight: '800',
     },
-    { path: "./WayFinderCF-Light.eot", weight: "300" },
-    { path: "./WayFinderCF-Light.woff", weight: "300" },
+    { path: './WayfinderCF-Heavy.eot', weight: '900' },
+    { path: './WayfinderCF-Heavy.woff', weight: '900' },
     {
-      path: "./WayFinderCF-Light.woff2",
-      weight: "300",
+      path: './WayfinderCF-Heavy.woff2',
+      weight: '900',
+    },
+    { path: './WayfinderCF-Light.eot', weight: '300' },
+    { path: './WayfinderCF-Light.woff', weight: '300' },
+    {
+      path: './WayfinderCF-Light.woff2',
+      weight: '300',
     },
   ],
-  variable: "--wayfinder-font",
-});
+  variable: '--wayfinder-font',
+})
